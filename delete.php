68c5a69bfc082bbc183d382a79aa817a4dcdcd7e
Nuri Akman <nuriakman@gmail.com>
2020-01-27 17:56:39 +0300

    Delete sayfası güncellendi

--- delete.php
+++ delete.php
@@ -1,15 +1,17 @@
 <?php
     include 'db_baglan.php';
 
-    if(isset($_GET["id"])){
-        // Sorgumuzu hazırlayalım
-        $SORGU = $DB->prepare("DELETE FROM rehber WHERE id=:id");
-        // Silinecek kayıt için ID değerini sorguda yerine koyalım
-        $SORGU->bindParam(":id", $_GET["id"]);
-        // Sorguyu çalıştıralım
-        $SORGU->execute();
-        // İşlem tamam. Ana sayfaya yönlendirelim.
-        header("location: index.php");
-        die();
+    if(!isset($_GET['id'])){
+        die("HATA: ID Değeri yok!");
     }
+
+    // Sorgumuzu hazırlayalım
+    $SORGU = $DB->prepare("DELETE FROM rehber WHERE id=:id");
+    // Silinecek kayıt için ID değerini sorguda yerine koyalım
+    $SORGU->bindParam(":id", $_GET["id"]);
+    // Sorguyu çalıştıralım
+    $SORGU->execute();
+    // İşlem tamam. Ana sayfaya yönlendirelim.
+    header("location: index.php");
+    die();
 ?>
